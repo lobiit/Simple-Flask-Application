20eda24b2722e349065ee54accf07e786c4b1302
Tumbo Lobi John <johnlobiit@gmail.com>
2022-11-15 16:34:27 +0300

    refactoring

--- app.py
+++ app.py
@@ -3,14 +3,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return render_template('index.html')
-
-
-@app.route('/greet', methods=['POST'])
-def greet():  # put application's code here
-    return render_template('greet.html', name=request.form.get("name", "rav"))
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        return render_template("greet.html", name=request.form.get("name", "lobi"))
+    if request.method == "GET":
+        return render_template("index.html")
 
 
 if __name__ == '__main__':
