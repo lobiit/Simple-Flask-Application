2eecb2f537fea230f5aaf579e51941e93a9daee9
Tumbo Lobi John <johnlobiit@gmail.com>
2022-11-15 16:05:59 +0300

    routing

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template('index.html', name=request.args.get("name", "rav"))
+    return render_template('index.html')
+
+
+@app.route('/greet')
+def greet():  # put application's code here
+    return render_template('greet.html', name=request.args.get("name", "rav"))
 
 
 if __name__ == '__main__':
