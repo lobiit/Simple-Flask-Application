4e80aee3582fdd4a02487eb8caf95cd8e38861e4
Tumbo Lobi John <johnlobiit@gmail.com>
2022-11-16 09:45:39 +0300

    generating a list to the user

--- app.py
+++ app.py
@@ -2,13 +2,24 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+SPORTS = [
+    "Football",
+    "Rugby",
+    "Volleyball",
+    "Basketball",
+]
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
-        return render_template("greet.html", name=request.form.get("name", "lobi"))
-    if request.method == "GET":
-        return render_template("index.html")
+    return render_template("index.html", sports=SPORTS)
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    if not request.form.get("name") or not request.form.get("sport") not in SPORTS:
+        return render_template("failure.html")
+    return render_template("success.html")
 
 
 if __name__ == '__main__':
