5202f68cd3d53b2d15b159e7768561f2cae35577
Tumbo Lobi John <johnlobiit@gmail.com>
2022-11-16 12:13:39 +0300

    login functionality

--- login/app.py
+++ login/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, session
+from flask import Flask, request, render_template, session, redirect
 from flask_session import Session
 
 app = Flask(__name__)
@@ -11,9 +11,14 @@ Session(app)
 
 @app.route("/")
 def index():
+    if not session.get("name"):
+        return redirect('/login')
     return render_template("base.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        session["name"]=request.form.get("name")
+        redirect('/')
     return render_template("login.html")
